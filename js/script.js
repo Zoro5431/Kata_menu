4a1a6f44baa3b5367c1380681183dd785f74d856
Zoro5431 <maloff.kirill@yandex.ru>
2025-01-12 14:52:34 +0300

    Внесены правки по комментариям

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 const iconMenu = document.querySelector('.toggle-nav');
 
 if (iconMenu){
-	const menuBody = document.querySelector('.menu');
-	const fence = document.querySelector('.logo');
-	const pageOverlay = document.querySelector('.page-overlay');
+	const menuBody = document.querySelector('.menu')
+	const fence = document.querySelector('.logo')
+	const pageOverlay = document.querySelector('.page-overlay')
 
-	iconMenu.addEventListener("click", function(e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-		fence.classList.toggle('_lock');
-		pageOverlay.classList.toggle('_lock');
-	});
+	/*
+	TODO: Реализовано временное решение для открытия меню по умолчанию.
+	На больших разрешениях лого и кнопка закрытия не отображается, будет доработано
+	на финальном макете.
+	*/
 
-	pageOverlay.addEventListener("click", function(e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-		fence.classList.toggle('_lock');
-		pageOverlay.classList.toggle('_lock');
-	});
+	document.body.classList.add('_lock')
+	iconMenu.classList.add('_active')
+	menuBody.classList.add('_active')
+	fence.classList.add('_lock')
+	pageOverlay.classList.add('_lock')
+
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+		fence.classList.toggle('_lock')
+		pageOverlay.classList.toggle('_lock')
+	})
+
+	pageOverlay.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+		fence.classList.toggle('_lock')
+		pageOverlay.classList.toggle('_lock')
+	})
 }
 
 const showText = document.querySelector('.main-content__read-more-button');
