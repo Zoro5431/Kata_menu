c06204150312e830a518f032d016bb7b7af3cca9
Zoro5431 <maloff.kirill@yandex.ru>
2025-01-22 18:36:32 +0300

    Реалезованы кнопки 'Показать всё'/'Скрыть', реализована секция набора брендов с использованием SwiperSlider

--- js/script.js
+++ js/script.js
@@ -44,3 +44,56 @@ if (showText){
 		showText.classList.toggle('_active');
 	});
 }
+
+function ScrollUp() {
+	let interval, countPx
+	countPx = document.body.scrollTop || window.pageYOffset
+	interval = setInterval(function () {
+		if (countPx > 0) window.scroll(0, (countPx -= 5))
+		else clearInterval(interval)
+	}, 5)
+}
+
+if (window.innerWidth < 768) {
+	new Swiper('.swiper', {
+		spaceBetween: 0,
+		// init: false,
+		resize: 0,
+		autoplay: {
+			delay: 7000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-custom-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+
+		slidesPerView: 'auto',
+		spaceBetween: 16,
+	})
+}
+
+const brandsDescription = document.querySelector('.brand-list.swiper')
+const brandsLimiter = document.querySelector(
+	'.brand-list__items.swiper-wrapper'
+)
+const shadowRight = document.querySelector('.brand-list__right-shadow')
+const shadowLeft = document.querySelector('.brand-list__left-shadow')
+
+if (window.innerWidth >= 768) {
+	const showBrands = document.querySelector('.brand-list__read-more-button')
+	brandsDescription.classList.toggle('_description')
+	brandsLimiter.classList.toggle('_limiter')
+	showBrands.classList.toggle('_on')
+	shadowRight.classList.toggle('_off')
+	shadowLeft.classList.toggle('_off')
+
+	if (showBrands) {
+		showBrands.addEventListener('click', function (e) {
+			brandsDescription.classList.toggle('_active')
+			brandsLimiter.classList.toggle('_active')
+			showBrands.classList.toggle('_active')
+		})
+	}
+}
